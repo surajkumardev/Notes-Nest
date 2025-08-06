66da218f38efbaec49fa029fa1f0e49100d75d77
surajkumardev <blackmesd@gmail.com>
2025-08-06 09:35:29 +0530

    adding files

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <button className="bg-red-200 text-4xl , border-amber-400,bg">
 
+        Delete
+      </button>
     </div>
   );
 }
